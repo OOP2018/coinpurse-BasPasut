bb26926856f8225157066fe2205494a95e860eda
Pasut Kittiprapas <pasut.ki@ku.th>
2018-01-20 15:58:00 +0700

    Complete Problem5

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -20,16 +20,35 @@ public class MoneyUtil {
 	 * @param c
 	 */
 	 public static void printCoins(List<Coin> c){
-	    	System.out.println(c.toString());
+	    	for(Coin coin : c) {
+	    		System.out.println(coin);
+	    	}
 	    }
 	
+	 public static List<Coin> filterByCurrency(List<Coin> coins, String currency){
+		 List<Coin> sortByCurrency = new ArrayList<Coin>();
+		 for(int i = 0 ; i < coins.size() ; i++) {
+			 if(coins.get(i).getCurrency().equalsIgnoreCase(currency)) {
+				 sortByCurrency.add(coins.get(i));
+			 }
+		 }
+		 return sortByCurrency;
+	 }
+	 
 	public static void main(String[] args) {
 		List<Coin> coins = new ArrayList<Coin>();
     	coins.add(new Coin(10.0,"Baht"));
     	coins.add(new Coin(50.5,"Baht"));
     	coins.add(new Coin(5.8,"Baht"));
+    	coins.add(new Coin(54.2,"Ringgit"));
+    	coins.add(new Coin(100,"Yuan"));
+    	coins.add(new Coin(12.4,"Ringgit"));
+    	
     	printCoins(coins);
+    	System.out.println("---------------------");
     	sortCoins(coins);
     	printCoins(coins);
+    	System.out.println("---------------------");
+    	printCoins(filterByCurrency(coins, "Ringgit"));
 	}
 }
